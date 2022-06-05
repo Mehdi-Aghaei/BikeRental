7703edd53860346096e696c858262d461617ac0e
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2022-06-05 21:38:40 +0200

    ShouldAddBikeAsync -> FAIL

--- BikeRental.Core.Tests.Unit/Services/Foundations/Bikes/BikeServiceTests.cs
+++ BikeRental.Core.Tests.Unit/Services/Foundations/Bikes/BikeServiceTests.cs
@@ -1,6 +1,40 @@
-﻿namespace BikeRental.Core.Tests.Unit.Services.Foundations.Bikes;
-public class BikeServiceTests
+﻿using BikeRental.Core.Brokers.Loggings;
+using BikeRental.Core.Brokers.Storages;
+using BikeRental.Core.Models.Bikes;
+using BikeRental.Core.Services.Foundations.Bikes;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace BikeRental.Core.Tests.Unit.Services.Foundations.Bikes;
+public partial class BikeServiceTests
 {
+    private readonly Mock<IStorageBroker> storageBrokerMock;
+    private readonly Mock<ILoggingBroker> loggingBrokerMock;
+    private readonly IBikeService bikeSevice;
+    
+    public BikeServiceTests()
+    {
+        this.storageBrokerMock = new Mock<IStorageBroker>();
+        this.loggingBrokerMock = new Mock<ILoggingBroker>();
+        this.bikeSevice = new BikeService(
+            this.storageBrokerMock.Object, 
+            this.loggingBrokerMock.Object);
+    }
 
+    private static DateTimeOffset GetRandomDateTimeOffset() =>
+        new DateTimeRange(earliestDate: new DateTime()).GetValue();
 
+    private static Bike CreateRandomBike() =>
+        CreateBikeFiller(date: GetRandomDateTimeOffset()).Create();
+
+    private static Filler<Bike> CreateBikeFiller(DateTimeOffset date)
+    {
+        var filler = new Filler<Bike>();
+        
+        filler.Setup()
+            .OnType<DateTimeOffset>()
+            .Use(date);
+        
+        return filler;
+    }
 }
--- BikeRental.Core/Services/Foundations/Bikes/BikeService.cs
+++ BikeRental.Core/Services/Foundations/Bikes/BikeService.cs
@@ -1,13 +1,16 @@
 ﻿using BikeRental.Core.Brokers.Loggings;
+using BikeRental.Core.Brokers.Storages;
 using BikeRental.Core.Models.Bikes;
 
 namespace BikeRental.Core.Services.Foundations.Bikes;
 public class BikeService : IBikeService
 {
+    private readonly IStorageBroker storageBroker;
     private readonly ILoggingBroker loggingBroker;
 
-    public BikeService(ILoggingBroker loggingBroker)
+    public BikeService(IStorageBroker storageBroker,ILoggingBroker loggingBroker )
     {
+        this.storageBroker = storageBroker;
         this.loggingBroker = loggingBroker;
     }
 

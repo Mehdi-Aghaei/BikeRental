c7ac19c79c5effddc474bec7b133b9f622d0f0af
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2022-06-17 11:25:35 +0200

    CONTROLLERS: Post bike controllers

---
+++ BikeRental.Core/Controllers/BikeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BikeRental.Core.Models.Bikes;
+using BikeRental.Core.Models.Bikes.Exceptions;
+using BikeRental.Core.Services.Foundations.Bikes;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace BikeRental.Core.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BikeController : RESTFulController
+{
+	private readonly IBikeService bikeService;
+
+	public BikeController(IBikeService bikeService)
+	{
+		this.bikeService = bikeService;
+	}
+
+    [HttpPost]
+    public async ValueTask<ActionResult<Bike>> PostBikeAsync(Bike bike)
+    {
+		try
+		{
+			Bike addedBike = 
+				await this.bikeService.AddBikeAsync(bike);
+
+			return Created(bike);
+		}
+		catch (BikeValidationException bikeValidationException)
+		{
+
+			return BadRequest(bikeValidationException.InnerException);
+		}
+        catch (BikeServiceException bikeServiceException)
+        {
+            return InternalServerError(bikeServiceException);
+        }
+    }
+}

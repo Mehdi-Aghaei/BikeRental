f4a3bdb2f8ef2819b8640dc09b97386faa781659
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2022-06-17 10:09:48 +0200

    ShouldThrowServiceExceptionOnAddIfServiceErrorOccursAndLogItAsync -> FAIL

---
+++ BikeRental.Core.Tests.Unit/Services/Foundations/Bikes/BikeServiceTests.Exceptions.Add.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BikeRental.Core.Models.Bikes;
+using BikeRental.Core.Models.Bikes.Exceptions;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using Xunit;
+
+namespace BikeRental.Core.Tests.Unit.Services.Foundations.Bikes;
+public partial class BikeServiceTests
+{
+    [Fact]
+    public async Task ShouldThrowServiceExceptionOnAddIfServiceErrorOccursAndLogItAsync()
+    {
+        // given
+        Bike someBike = CreateRandomBike();
+        Exception serviceException = new Exception();
+
+        var failedBikeServiceException =
+            new FailedBikeServiceException(serviceException);
+
+        var expectedBikeServiceException =
+            new BikeServiceException(failedBikeServiceException);
+
+        this.storageBrokerMock.Setup(broker =>
+            broker.InsertBikeAsync(someBike))
+                .ThrowsAsync(serviceException);
+
+        // when
+        ValueTask<Bike> addBikeTask =
+            this.bikeSevice.AddBikeAsync(someBike);
+
+        // then
+        await Assert.ThrowsAsync<BikeServiceException>(() => addBikeTask.AsTask());
+
+        this.storageBrokerMock.Verify(broker =>
+            broker.InsertBikeAsync(someBike), 
+                Times.Once);
+
+        this.loggingBrokerMock.Verify(broker =>
+            broker.LogError(It.Is(
+                SameExceptionAs(expectedBikeServiceException))),
+                    Times.Once);
+
+        this.storageBrokerMock.VerifyNoOtherCalls();
+        this.loggingBrokerMock.VerifyNoOtherCalls();
+    }
+}
+
---
+++ BikeRental.Core/Models/Bikes/Exceptions/FailedBikeServiceException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xeptions;
+
+namespace BikeRental.Core.Models.Bikes.Exceptions;
+public class FailedBikeServiceException : Xeption
+{
+	public FailedBikeServiceException(Exception innerException)
+		: base(message: "Failed bike service error occurred, contact support.", innerException)
+	{ }
+}

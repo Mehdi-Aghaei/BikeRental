3e67bcfe2ab283ae0c8222d512822cf9e8adc6fc
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2022-07-14 11:17:29 +0200

    ShouldAddCustomerAsync -> FAIL

---
+++ BikeRental.Core.Tests.Unit/Services/Foundations/Customers/CustomerServiceTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BikeRental.Core.Brokers.Loggings;
+using BikeRental.Core.Brokers.Storages;
+using BikeRental.Core.Models.Customers;
+using BikeRental.Core.Services.Foundations.Customers;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace BikeRental.Core.Tests.Unit.Services.Foundations.Customers;
+public partial class CustomerServiceTests
+{
+    private readonly Mock<IStorageBroker> storageBrokerMock;
+    private readonly Mock<ILoggingBroker> loggingBrokerMock;
+    private readonly ICustomerService customerService;
+
+    public CustomerServiceTests()
+    {
+        this.storageBrokerMock = new Mock<IStorageBroker>();
+        this.loggingBrokerMock = new Mock<ILoggingBroker>();
+        this.customerService = new CustomerService(
+            this.storageBrokerMock.Object,
+            this.loggingBrokerMock.Object);
+    }
+
+    private static DateTimeOffset GetRandomDateTimeOffset() =>
+       new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+    private static Customer CreateRandomCustomer() =>
+        CreateCustomerFiller(date: GetRandomDateTimeOffset()).Create();
+
+    private static Filler<Customer> CreateCustomerFiller(DateTimeOffset date)
+    {
+        var filler = new Filler<Customer>();
+
+        filler.Setup().OnType<DateTimeOffset>()
+            .Use(date);
+
+        return filler;
+    }
+}
---
+++ BikeRental.Core/Services/Foundations/Customers/CustomerService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BikeRental.Core.Brokers.Loggings;
+using BikeRental.Core.Brokers.Storages;
+using BikeRental.Core.Models.Customers;
+
+namespace BikeRental.Core.Services.Foundations.Customers;
+public class CustomerService : ICustomerService
+{
+    private readonly IStorageBroker storageBroker;
+    private readonly ILoggingBroker loggingBroker;
+
+    public CustomerService(IStorageBroker storageBroker, ILoggingBroker loggingBroker)
+    {
+        this.storageBroker = storageBroker;
+        this.loggingBroker = loggingBroker;
+    }
+
+    public ValueTask<Customer> AddCustomerAsync(Customer customer)
+    {
+        throw new NotImplementedException();
+    }
+}
